77d24e7f13c3572cdf877477bfcfba3a8e661d3f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-13 10:54:01 +0530

    Added QuizSection component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import QuizSection from "./components/Quiz/QuizSection";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <QuizSection></QuizSection>
+    </div>
+  );
 }
 
 export default App;
