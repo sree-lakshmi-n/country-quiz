50a668738017f2d8ef549563e2d8bf434c391052
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-13 14:16:13 +0530

    QuizIllustration component

--- src/components/Quiz/QuizSection.js
+++ src/components/Quiz/QuizSection.js
@@ -1,17 +1,12 @@
 import "./QuizSection.css";
 import Container from "../UI/Container";
-import quizIllustration from "../../images/undraw_adventure_4hum 1.svg";
+import QuizIllustration from "./QuizIllustration";
 const QuizSection = () => {
   return (
     <div className="wrapper">
       <h1 className="title">Country Quiz</h1>
       <Container>
-        <div className="illustration-container">
-          <img
-            src={quizIllustration}
-            alt="illustration showing a person travelling around the world"
-          />
-        </div>
+        <QuizIllustration />
         <p className="question"></p>
       </Container>
     </div>
