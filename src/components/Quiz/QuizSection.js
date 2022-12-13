7dcf37ac7b971a2568fe2a40879ee146bd496c29
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-13 10:54:45 +0530

    Added wrapper div and Container component

---
+++ src/components/Quiz/QuizSection.js
@@ -0,0 +1,13 @@
+import "./QuizSection.css";
+import Container from "../UI/Container";
+const QuizSection = () => {
+  return (
+    <div className="wrapper">
+      <h1 className="title">Country Quiz</h1>
+      <Container>
+        <p>hey!</p>
+      </Container>
+    </div>
+  );
+};
+export default QuizSection;
