68aa63ce6f0ff20891c6a1538525acf7e69addc8
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-16 10:29:05 +0530

    Options component

---
+++ src/components/Quiz/Options.js
@@ -0,0 +1,14 @@
+import "./Options.css";
+const Options = () => {
+  return (
+    <p className="option">
+      <span className="option-num">A</span>
+      <span className="option-country">Vietnam</span>
+      <span className="option-result">
+        <ion-icon name="close-circle-outline"></ion-icon>
+        <ion-icon name="checkmark-circle-outline"></ion-icon>
+      </span>
+    </p>
+  );
+};
+export default Options;
