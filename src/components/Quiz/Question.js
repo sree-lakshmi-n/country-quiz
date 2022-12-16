5bf2353343d6f3cd92b4cf9622ad1e6d11ef0e89
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-16 10:33:22 +0530

    Question component

---
+++ src/components/Quiz/Question.js
@@ -0,0 +1,10 @@
+import "./Question.css";
+const Question = () => {
+  return (
+    <div className="question">
+      <img className="flag" src="" />
+      <p className="question-content">Kuala Lumpur is the capital of </p>
+    </div>
+  );
+};
+export default Question;
