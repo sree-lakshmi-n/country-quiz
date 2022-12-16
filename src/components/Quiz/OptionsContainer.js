2c402a360f0e33f6398e804f3960371ee710941e
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-12-16 10:32:52 +0530

    OptionsContainer component

---
+++ src/components/Quiz/OptionsContainer.js
@@ -0,0 +1,13 @@
+import "./OptionsContainer.css";
+import Options from "./Options";
+const OptionsContainer = () => {
+  return (
+    <div className="options-container">
+      <Options />
+      <Options />
+      <Options />
+      <Options />
+    </div>
+  );
+};
+export default OptionsContainer;
